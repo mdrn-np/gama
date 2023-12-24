dcd07f5df4706184fe628121916e2beff0386b76
Gaurav Phuyal <phuyalgaurav90@gmail.com>
2023-12-25 01:58:51 +0545

    added extra domain filtering

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 import uvicorn
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 import joblib
+from helpers import get_domain_name
 
 app = FastAPI()
 
@@ -10,18 +11,17 @@ phish_model_ls = joblib.load(phish_model)
 
 # API with prediction
 @app.get('/phishing')
-async def predict(url: str = Query(...,min_length=1,max_length=2083)):
+async def predict(url: str):
 	X_predict = []
-	X_predict.append(str(url))
+	domain = get_domain_name(url)
+	X_predict.append(str(domain))
 	y_Predict = phish_model_ls.predict(X_predict)
 	if y_Predict == 'bad':
 		result = True
 	else:
 		result = False
-
 	return (result)
 
 # Run API with uvicorn
 if __name__ == '__main__':
 	uvicorn.run(app,host="127.0.0.1",port=8000)
-    
