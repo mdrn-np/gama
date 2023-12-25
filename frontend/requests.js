d42227017132c2671c5ffec479fc77561b539631
Gaurav Phuyal <phuyalgaurav90@gmail.com>
2023-12-25 19:40:37 +0545

    change request.js

--- frontend/requests.js
+++ frontend/requests.js
@@ -1,12 +1,12 @@
 site = "http://localhost:8000/";
 
-async function checkPhishing(url) {
+export async function checkPhishing(url) {
   const site_url = `${site}phishing?url=${url}`;
 
   await fetch(site_url)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      return data;
     })
     .catch((error) => {
       console.error(error);
@@ -14,16 +14,18 @@ async function checkPhishing(url) {
 }
 
 // test
-checkPhishing(
-  "https://www.goodd-gler222.co/search?q=convert+a+url+string+into+domain+name+and+tld&rlz=1C1ONGR_enNP1088NP1088&oq=convert+a+url+string+into+domain+name+and+tld&gs_lcrp=EgZjaHJvbWUyCAgAEEUYFRg5MgcIARAhGKABMgcIAhAhGKABMgcIAxAhGKAB0gEJMTQwMTFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+console.log(
+  checkPhishing(
+    "https://www.goodd-gler222.co/search?q=convert+a+url+string+into+domain+name+and+tld&rlz=1C1ONGR_enNP1088NP1088&oq=convert+a+url+string+into+domain+name+and+tld&gs_lcrp=EgZjaHJvbWUyCAgAEEUYFRg5MgcIARAhGKABMgcIAhAhGKABMgcIAxAhGKAB0gEJMTQwMTFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+  )
 );
-async function getWebDetails(url) {
+export async function getWebDetails(url) {
   const site_url = `${site}details?url=${url}`;
 
   await fetch(site_url)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      return data;
     })
     .catch((error) => {
       console.error(error);
@@ -31,5 +33,5 @@ async function getWebDetails(url) {
 }
 
 // test
-getWebDetails("theintercept.com"); // dnssec is true
-getWebDetails("google.com"); // dnssec is false
+console.log(getWebDetails("theintercept.com")); // dnssec is true
+console.log(getWebDetails("google.com")); // dnssec is false
