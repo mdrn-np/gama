7fa0cd86598abc47cd8c7953061f26e2c5f722d4
Gaurav Phuyal <phuyalgaurav90@gmail.com>
2023-12-25 08:21:56 +0545

    Add database connection and CRUD operations for phishing reports

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,21 @@ import uvicorn
 from fastapi import FastAPI
 import joblib
 from helpers import get_domain_name
+from models import PhishingReport
+from tortoise.contrib.fastapi import register_tortoise
+from models import PhishingReportSchema
+
 
 app = FastAPI()
 
+# Connect to database
+register_tortoise(
+    app,
+    db_url="sqlite://db.sqlite3",
+    modules={"models": ["models"]},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
 # Load Model
 phish_model = open('phishing.pkl','rb')
 phish_model_ls = joblib.load(phish_model)
@@ -16,11 +28,41 @@ async def predict(url: str):
 	domain = get_domain_name(url)
 	X_predict.append(str(domain))
 	y_Predict = phish_model_ls.predict(X_predict)
+	reports = await PhishingReport.filter(real=True).all()
+	urls = [report.url for report in reports]
+	if domain in urls:
+		result = True
 	if y_Predict == 'bad':
 		result = True
 	else:
 		result = False
 	return (result)
+
+# report phishing and save to database
+@app.post('/report')
+async def report(report: PhishingReportSchema):
+	domain = get_domain_name(report.url)
+	reports = await PhishingReport.all()
+	urls = [report.url for report in reports]
+	if domain in urls:
+		return {'result': 'already reported'}
+	elif not domain:
+		return {'result': 'invalid url'}
+	report = PhishingReport(url=domain, reason=report.reason)
+	await report.save()
+	return {'result': 'success'}
+
+#get all reports
+@app.get('/reports')
+async def reports():
+	reports = await PhishingReport.all()
+	return reports
+
+#update report
+@app.put('/reports/{id}')
+async def update(id: int, real: bool):
+	await PhishingReport.filter(id=id).update(real=real)
+	return {'result': 'success'}
 
 # Run API with uvicorn
 if __name__ == '__main__':
