e9a8117640a762322b7f32de3c3bc99cc49ec322
Gaurav Phuyal <phuyalgaurav90@gmail.com>
2023-12-25 19:24:49 +0545

    Refactor checkPhishing function to use async/await and add getWebDetails function

--- frontend/requests.js
+++ frontend/requests.js
@@ -1,9 +1,9 @@
 site = "http://localhost:8000/";
 
-export function checkPhishing(url) {
+async function checkPhishing(url) {
   const site_url = `${site}phishing?url=${url}`;
 
-  fetch(site_url)
+  await fetch(site_url)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
@@ -14,6 +14,22 @@ export function checkPhishing(url) {
 }
 
 // test
-sendGetRequest(
+checkPhishing(
   "https://www.goodd-gler222.co/search?q=convert+a+url+string+into+domain+name+and+tld&rlz=1C1ONGR_enNP1088NP1088&oq=convert+a+url+string+into+domain+name+and+tld&gs_lcrp=EgZjaHJvbWUyCAgAEEUYFRg5MgcIARAhGKABMgcIAhAhGKABMgcIAxAhGKAB0gEJMTQwMTFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
 );
+async function getWebDetails(url) {
+  const site_url = `${site}details?url=${url}`;
+
+  await fetch(site_url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+// test
+getWebDetails("theintercept.com"); // dnssec is true
+getWebDetails("google.com"); // dnssec is false
