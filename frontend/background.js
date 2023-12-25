e1f4edb98639c53e54eed257fedf90044fbe8a91
Manish Bagale <manish.bagale3@gmail.com>
2023-12-25 21:45:36 +0545

    added code to check the current active tab

--- frontend/background.js
+++ frontend/background.js
@@ -1,1 +1,6 @@
-import { checkPhishing, getWebDetails } from "./requests";
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  // returns active tab only
+  let activeTab = tabs[0];
+  let activeTabUrl = activeTab.url
+  console.log(activeTabUrl)
+});
