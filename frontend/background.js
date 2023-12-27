d34499306926df4e62e85a52d0c1d0b6e2ad07f2
Gaurav Phuyal <phuyalgaurav90@gmail.com>
2023-12-27 19:56:50 +0545

    Refactor background.js to fetch phishing URL and add context menu

--- frontend/background.js
+++ frontend/background.js
@@ -1,26 +1,41 @@
+let activeTabUrl = "";
+
 chrome.tabs.onActivated.addListener(() => {
   // returns active tab's url
-  chrome.tabs.query({highlighted: true}, (tabs) => {
-    let activeTab = tabs[0]
-    let activeTabUrl = activeTab.url;
-    console.log(activeTabUrl)
-  })
-})
+  chrome.tabs.query({ highlighted: true }, (tabs) => {
+    let activeTab = tabs[0];
+    activeTabUrl = activeTab.url;
+    console.log(activeTabUrl);
+    const server_url = "http://localhost:8000/";
+
+    async function checkPhishing(url) {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    let phishing_url = `${server_url}phishing?url=${activeTabUrl}`;
+    let phishing = checkPhishing(phishing_url);
+  });
+});
 
 chrome.runtime.onInstalled.addListener(() => {
   // adds context menu needed for our extension on installation
   chrome.contextMenus.create({
-    "id": "validate",
-    "title": "validate",
-    "contexts": ["selection"]
-  })
-  console.log('test')
-})
+    id: "validate",
+    title: "validate",
+    contexts: ["selection"],
+  });
+  console.log("test");
+});
 
 chrome.contextMenus.onClicked.addListener((clickData) => {
   // extracts the selected / highlighted value
   if (clickData.menuItemId == "validate" && clickData.selectionText) {
     let review = clickData.selectionText;
-    console.log(review)
+    console.log(review);
   }
-})
+});
